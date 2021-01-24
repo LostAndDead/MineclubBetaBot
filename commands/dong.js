b95f0a499d8502219aeb65d98d25283c84686581
LostAndDead <jakemarsh9012@gmail.com>
2021-01-24 10:23:18 +0000

    fixed shlong again

--- commands/dong.js
+++ commands/dong.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
+
 module.exports.run = async(bot, message, args) => {
     let num = Math.floor(Math.random() * 11);
     let shlong = ""
     while (num > 0){
-        shlong =+ "="
-        num--
+        shlong += "=";
+        num--;
     }
     message.channel.send(`Your dick is:\n8${shlong}D`)
 };
