2950b6ad92af033eb4d2b653635a29ea94fe9981
LostAndDead <jakemarsh9012@gmail.com>
2021-04-09 15:48:49 +0100

    updated to become beta bot

--- index.js
+++ index.js
@@ -53,10 +53,14 @@ process.on('warning', console.warn);
 
 //on ready statment
 bot.on("ready", async() => {
-    console.log("\nThe bot is now online")
-    console.log("Keep this window open for the bot to run\n")
-    console.log(`Invite me to a server with the following link.\nhttps://discordapp.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=125952&scope=bot\n`);
-    console.log("Press CTRL+C to exit\n")
+    console.log("Online!")
+    console.log("^ The above message is for Pterodactyl to pickup and mark the server as online. ^\n")
+    console.log("------------------------------------------------------------------------------------------------------")
+    console.log("The bot is now online")
+    console.log(`Logged in as ${bot.user.username} || ${bot.user.id}`)
+    console.log("------------------------------------------------------------------------------------------------------")
+    console.log(`Invite me to a server with the following link.\nhttps://discordapp.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=125952&scope=bot`);
+    console.log("------------------------------------------------------------------------------------------------------")
     bot.user.setPresence({
         status: "online",
         activity: {
@@ -76,29 +80,11 @@ bot.on("message", async message => {
         let Commandfile = bot.commands.get(cmd.slice(Config.Setup.Prefix.length));
         if (Commandfile) Commandfile.run(bot, message, args);
     }else{
-        let contains = "#stairgang"
-        let contains2 = "#**stairgang**"
-        if (message.content.toLowerCase().includes(contains) || message.content.toLowerCase().includes(contains2)){
-            message.channel.send("#**StairGang**")
-        }else{
-            let num = Math.floor(Math.random() * 101);
-            if(num == 69){
-                message.channel.send("Thats a **#StairGang** moment.")
-            }
+        let num = Math.floor(Math.random() * 101);
+        if(num == 69){
+            message.channel.send("Thats a **#Beta** moment.")
         }
     }
 });
-
-bot.on('guildMemberAdd', member => {
-    if (member.guild.id == "802159815706542100"){
-        channel = member.guild.channels.cache.find(i => i.id === '802159815706542103')
-        channel.send(`
-Welcome <@${member.id}>! ❤️ 
-<#802160845911621644> for the story of the gang
-<#802163360270778378> You need to change your title color to join the gang BTW 🤌 
-<#802720998603489359>  for news & updates on the server
-        `)
-    }
-})
 
 bot.login(Config.Setup.Token);
